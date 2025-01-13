c5e7b60765e99021c5a1d9e89b04a61c2ecd0c42
charines <charles@inmade.com.br>
2025-01-13 07:05:04 -0300

    ajustando largura do video e o click para calcular o ipva vai para o video do reinando

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,36 +2,36 @@ function Footer() {
   return (
     <footer className="py-6 bg-gray-800 text-white text-center">
       <p className="text-lg mb-4">Siga-nos nas redes sociais:</p>
-      <div className="space-x-4">
+      <div className="flex flex-wrap justify-center gap-4">
         <a
-          href="https://www.instagram.com/dsopoficial"
+          href="https://www.instagram.com/dsop.oficial/"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline"
+          className="underline hover:text-gray-300"
         >
           Instagram
         </a>
         <a
-          href="https://www.facebook.com/dsopoficial"
+          href="https://www.facebook.com/DSOPEducacaoFinanceira"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline"
+          className="underline hover:text-gray-300"
         >
           Facebook
         </a>
         <a
-          href="https://www.linkedin.com/company/dsop-educacao-financeira"
+          href="https://www.linkedin.com/company/dsopoficial/"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline"
+          className="underline hover:text-gray-300"
         >
           Linkedin
         </a>
         <a
-          href="https://www.youtube.com/@dinheiroavista"
+          href="https://www.youtube.com/c/Dinheiro%C3%A0vista"
           target="_blank"
           rel="noopener noreferrer"
-          className="underline"
+          className="underline hover:text-gray-300"
         >
           YouTube Dinheiro à Vista
         </a>
